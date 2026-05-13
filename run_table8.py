dc79fbe3ed66ee7eae3425602b2a9bcbb005cbe8
Max Feldman <mackx@protonmail.com>
2026-05-13 00:11:39 -0500

    strange memory hack

--- run_table8.py
+++ run_table8.py
@@ -70,10 +70,10 @@ for d in ds:
         X    = torch.from_numpy(X_np)
         Xn   = torch.empty((N, m), dtype=torch.float64)
         Dloc = torch.zeros((N, m, b), dtype=torch.float64)
-        Gidx = torch.full((N, m, k), -1,                             dtype=torch.int32)
-        Gdst = torch.full((N, m, k), torch.finfo(torch.float64).max, dtype=torch.float64)
-        Lidx = torch.full((N, m, k), -1,                             dtype=torch.int32)
-        Ldst = torch.full((N, m, k), torch.finfo(torch.float64).max, dtype=torch.float64)
+        Gidx = torch.full((N, m, k + 1), -1,                             dtype=torch.int32)
+        Gdst = torch.full((N, m, k + 1), torch.finfo(torch.float64).max, dtype=torch.float64)
+        Lidx = torch.full((N, m, k + 1), -1,                             dtype=torch.int32)
+        Ldst = torch.full((N, m, k + 1), torch.finfo(torch.float64).max, dtype=torch.float64)
 
         if args.pipeline == "fused" and k == b:
             from knn_kokkos_keqb import run_knn_pipeline_keqb
